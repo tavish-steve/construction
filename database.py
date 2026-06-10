5b3ee965794619d2df5f2409cb800db5250a0b41
tavish-steve <tavishsteve@gmail.com>
2026-06-10 22:43:29 +0300

    u

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 DB_CONFIG = {
     'host': os.environ.get('DB_HOST', '127.0.0.1'),
     'port': os.environ.get('DB_PORT', '5432'),
-    'user': os.environ.get('DB_USER', 'posgres'),
+    'user': os.environ.get('DB_USER', 'postgres'),
     'password': os.environ.get('DB_PASSWORD', 'rty67jouj'),
     'database': os.environ.get('DB_NAME', 'construction_db')
 }
